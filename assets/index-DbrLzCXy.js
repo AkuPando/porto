aa4236d36c6c1b92d83067c4d8e1cc3ae1114a40
AkuPando <irvandaaldo666@gmail.com>
2025-12-18 01:39:23 +0700

    Updates

--- assets/index-DbrLzCXy.js
+++
@@ -1,1 +0,0 @@
-import{R as t,j as o}from"./index-D7j0rl8A.js";const r=()=>o.jsx("div",{children:"product"}),s=t.memo(r);export{s as default};
--- assets/index-GgJYf72n.js
+++
@@ -1,1 +0,0 @@
-import{R as t,j as e}from"./index-D7j0rl8A.js";const o=()=>e.jsx("div",{children:"project"}),s=t.memo(o);export{s as default};
