045e03124403aadf32766729197869dfbf0152e1
AkuPando <irvandaaldo666@gmail.com>
2025-12-18 01:07:07 +0700

    Updates

---
+++ assets/index-D7wBGqbM.js
@@ -0,0 +1,1 @@
+import{R as t,j as o}from"./index-BvSP5hQ5.js";const r=()=>o.jsx("div",{children:"product"}),s=t.memo(r);export{s as default};
---
+++ assets/index-DO7GYhPP.js
@@ -0,0 +1,1 @@
+import{R as t,j as o}from"./index-BvSP5hQ5.js";const e=()=>o.jsx("div",{children:"contact"}),s=t.memo(e);export{s as default};
